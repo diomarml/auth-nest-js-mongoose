5c5cb9272741de732af79659a31b8f3eed1e727c
Diomar Martinez <diomarml@gmail.com>
2021-09-20 15:29:27 -0500

    Initial Dependecies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
+import * as csurf from 'csurf';
+import * as compression from 'compression';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(3000);
+  app.use(helmet())
+  app.use(csurf());
+  app.use(compression());
+
+
 }
 bootstrap();
